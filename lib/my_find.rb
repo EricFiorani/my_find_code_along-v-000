52b18b4664314c83c56a60c9cc80d0f427c84c08
Eric Fiorani <efiorani305@gmail.com>
2018-04-24 15:40:13 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ collection = (1..100).to_a
 def my_find(collection)
   i = 0
   while i < collection.length
-    yield(collection[i])
+    return collection[i] if yield(collection[i])
     i += 1
   end
 end
